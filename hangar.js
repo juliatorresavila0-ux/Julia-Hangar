c5a195fc7313012320162898411854a91ac0d6d7
juliatorresavila0-ux <juliatorresavila0@gmail.com>
2026-09-04 16:39:10 -0300

    Update hangar.js

--- hangar.js
+++ hangar.js
@@ -1,5 +1,5 @@
 const cartas=[
-{valor:"A",nome:"Pulso Rosa",dano:5},{valor:"2",nome:"Impacto",dano:3},{valor:"3",nome:"Laser",dano:3},{valor:"4",nome:"Rajada",dano:4},{valor:"5",nome:"Choque",dano:4},{valor:"6",nome:"Neon Beam",dano:5},{valor:"7",nome:"Combo",dano:5},{valor:"8",nome:"Overdrive",dano:6},{valor:"9",nome:"Photon",dano:6},{valor:"10",nome:"Pink Burst",dano:7},{valor:"J",nome:"Jack Strike",dano:8},{valor:"Q",nome:"Queen Beam",dano:9},{valor:"K",nome:"King Nova",dano:10}
+{valor:"A",nome:"Soco",dano:4},{valor:"2",nome:"Soco",dano:4},{valor:"3",nome:"Soco",dano:4},{valor:"4",nome:"Martelo Femur",dano:4-8},{valor:"5",nome:"Soco",dano:4},{valor:"6",nome:"Soco",dano:4),{valor:"7",nome:"Soco",dano:4},{valor:"8",nome:"Martelo Femur",dano:4-8},{valor:"9",nome:"Soco",dano:4},{valor:"10",nome:"Soco",dano:7},{valor:"11",nome:"Soco",dano:4},{valor:"12",nome:"Lingua",dano:4},{valor:"8",nome:"Codex",dano:3},{valor:"J",nome:"Jack Strike",dano:8},{valor:"Q",nome:"Queen Beam",dano:9},{valor:"K",nome:"King Nova",dano:10}
 ];
 const grid=document.getElementById("cartas");
 cartas.forEach(c=>{const el=document.createElement("article");el.className="carta";el.innerHTML=`<span class="carta-valor">${c.valor}</span><span class="carta-nome">${c.nome}</span><span class="carta-dano">Dano: <strong>${c.dano}</strong></span>`;grid.appendChild(el);});
